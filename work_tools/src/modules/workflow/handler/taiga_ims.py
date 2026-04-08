d1e5659b1eb5956523ecc4cffc0076fc8c85725f
minjae <kwj4479@gmail.com>
2026-04-08 23:03:55 +0900

    feat: Add API specification generator and Taiga IMS workflow handler

---
+++ work_tools/src/modules/workflow/handler/taiga_ims.py
@@ -0,0 +1,5 @@
+class TaigaImsWorkflowHandler:
+    """Taiga IMS workflow handler. (Facade for Taiga-IMS integration)
+
+    TODO: Taiga에서 데이터 조회해서, 연관 IMS의 uuid 리스트 추출해서 Taiga-IMS 연동을 구현할 예정
+    """
